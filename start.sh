992998790159328f69775300de0e36d940331eb0
vincenkaze <vincenkaze@proton.me>
2026-03-12 21:24:36 +0530

    Remove local lavalink startup

--- start.sh
+++ start.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-echo "Downloading Lavalink..."
-mkdir -p bot/lavalink
-cd bot/lavalink
-
-curl -L -o Lavalink.jar https://github.com/lavalink-devs/Lavalink/releases/latest/download/Lavalink.jar
-
-echo "Starting Lavalink..."
-java -jar Lavalink.jar &
-
-sleep 5
-
 echo "Starting VinRadio..."
-cd ../..
 node index.js
