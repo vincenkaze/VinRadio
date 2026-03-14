326f4fcc363398f835111eb9387f3d15ae0b4702
vincenkaze <vincenkaze@proton.me>
2026-03-14 19:01:25 +0530

    make start script executable

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "Starting VinRadio..."
-node index.js
+cd /tmp
+
+wget -q https://github.com/lavalink-devs/Lavalink/releases/download/4.2.2/Lavalink.jar -O Lavalink.jar
+
+cp /app/bot/lavalink/application.yml ./application.yml
+
+java -jar Lavalink.jar
